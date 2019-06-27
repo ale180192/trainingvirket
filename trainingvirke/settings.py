13700e798d62153227c245a06128117d61c20f60
Alejandro Lujan <ale@deploy.com>
2019-06-27 11:03:31 -0500

    add admindocs

--- trainingvirke/settings.py
+++ trainingvirke/settings.py
@@ -83,6 +83,14 @@ WSGI_APPLICATION = 'trainingvirke.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -93,12 +101,8 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+
+
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
 
